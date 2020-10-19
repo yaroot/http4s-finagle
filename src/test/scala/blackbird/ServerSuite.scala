2faa21270b064efc5b970534b72a18ea5ef78bf5
Yan Su <tsu@yaroot.net>
2020-10-19 17:44:39 +0900

    .

--- src/test/scala/blackbird/ServerSuite.scala
+++ src/test/scala/blackbird/ServerSuite.scala
@@ -4,17 +4,19 @@ import java.net.InetSocketAddress
 
 import cats.effect._
 import com.twitter.finagle
+import com.twitter.finagle.Http
 import com.twitter.util.{Await, Duration}
 import org.http4s.client.blaze.BlazeClientBuilder
 
 import scala.concurrent.ExecutionContext
 
-class ServerSuite extends munit.CatsEffectSuite with RouteSuite {
+trait ServerSuite extends munit.CatsEffectSuite with RouteSuite {
   val server = Blackbird.serveRoutes[IO](
     "localhost:0",
     TestRoutes.routes[IO],
-    finagle.Http.server.withRequestTimeout(Duration.fromSeconds(5))
+    makeServer
   )
+  def makeServer: finagle.Http.Server
 
   val port: Int       = server.boundAddress.asInstanceOf[InetSocketAddress].getPort
   val address: String = s"http://localhost:${port}"
@@ -25,4 +27,12 @@ class ServerSuite extends munit.CatsEffectSuite with RouteSuite {
     Await.result(server.close())
     shutdownClient.unsafeRunSync()
   }
+}
+
+class StreamingServerSuite extends ServerSuite {
+  def makeServer: Http.Server = finagle.Http.server.withRequestTimeout(Duration.fromSeconds(5)).withStreaming(true)
+}
+
+class NonStreamingServerSuite extends ServerSuite {
+  def makeServer: Http.Server = finagle.Http.server.withRequestTimeout(Duration.fromSeconds(5))
 }
