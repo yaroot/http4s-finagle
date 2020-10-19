27d0e773d7ca55058b8853f4eb2b89614a76e66d
Yan Su <tsu@yaroot.net>
2020-10-19 10:30:44 +0900

    .

--- src/main/scala/blackbird/impl/package.scala
+++ src/main/scala/blackbird/impl/package.scala
@@ -63,24 +63,23 @@ object Impl {
     response: FResponse
   ): F[Response[F]] = {
     val resp = for {
-      statusCode  <- Status.fromInt(response.statusCode)
+      statusCode <- Status.fromInt(response.statusCode)
       headers     = response.headerMap.toList.map { case (k, v) => Header(k, v).parsed }
       httpVersion = toHVersion(response.version)
-    } yield
-      Response[F](
-        statusCode,
-        httpVersion,
-        Headers(headers),
-        liftMessageBody(response)
-      )
+    } yield Response[F](
+      statusCode,
+      httpVersion,
+      Headers(headers),
+      liftMessageBody(response)
+    )
 
     ConcurrentEffect[F].fromEither(resp)
   }
 
   def fromFinagleRequest[F[_]: ConcurrentEffect](req: FRequest): Either[ParseFailure, Request[F]] =
     for {
-      method       <- Method.fromString(req.method.name)
-      uri          <- Uri.fromString(req.uri)
+      method      <- Method.fromString(req.method.name)
+      uri         <- Uri.fromString(req.uri)
       headers      = req.headerMap.toList.map { case (k, v) => Header(k, v).parsed }
       version      = toHVersion(req.version)
       twitterLocal = Local.save()
@@ -125,7 +124,7 @@ object Impl {
   def mkService[F[_]: ConcurrentEffect](app: HttpApp[F], streaming: Boolean): Svc[FRequest, FResponse] =
     Svc.mk[FRequest, FResponse] { freq =>
       fromFinagleRequest(freq) match {
-        case Left(exc) => Future.exception[FResponse](exc)
+        case Left(exc)  => Future.exception[FResponse](exc)
         case Right(req) =>
           Converter
             .unsafeRunAsync(app.run(req))
@@ -134,7 +133,8 @@ object Impl {
     }
 
   def mkClient[F[_]](service: Svc[FRequest, FResponse], streaming: Boolean)(implicit
-                                                                            F: ConcurrentEffect[F]): Client[F] = {
+    F: ConcurrentEffect[F]
+  ): Client[F] = {
     val execute: Request[F] => F[Response[F]] = { req: Request[F] =>
       fromHttp4sRequest(req, streaming)
         .flatMap { freq =>
@@ -165,7 +165,7 @@ object Impl {
                 }
             }
             .flatMap(toHttp4sResponse(_))
-        case None =>
+        case None    =>
           Sync[F].raiseError[Response[F]](new IllegalArgumentException(s"Illegal URL ${req.uri.toString()}"))
       }
     }
@@ -224,8 +224,8 @@ object Impl {
 
   implicit class readerOps[A](private val reader: Reader[A]) extends AnyVal {
     def collectBodyContent[F[_]](implicit F: ConcurrentEffect[F], ev: A =:= Buf): EntityBody[F] = {
-      val _ = ev
-      val r = reader.asInstanceOf[Reader[Buf]]
+      val _    = ev
+      val r    = reader.asInstanceOf[Reader[Buf]]
       val buf0 = F.delay {
         Reader
           .toAsyncStream(r)
